a3dac98e69e078c09fc8c0665834f9cb63a22768
tsingcoo <tsingcoo.wang@gmail.com>
2017-01-14 17:45:23 +0800

    生成一个DP数组，复杂度为N^2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include <vector>
 
+std::vector<int> GetDP1(std::vector<int>& arr){
+    std::vector<int> dp1;
+    for(int i = 0; i < arr.size(); ++i){
+        dp1.push_back(1);
+        for(int j = 0; j < i; ++j){
+            if(arr[j]<arr[i]){
+                dp1[i] = std::max(dp1[i], dp1[j]+1);
+            }
+        }
+    }
+}
 
 int main() {
 //    std::cout << "Hello, World!" << std::endl;
